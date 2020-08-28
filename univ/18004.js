04016bf03e04f426c13dd324107361b067031b1e
LiuWX <70356358+LWX99@users.noreply.github.com>
2020-08-28 04:09:43 +0000

    Create 18004.js
    华北水利水电大学

---
+++ univ/18004.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "华北水利水电大学",
+    id: "18004",
+    link: [ {
+        name: "华水主页",
+        url: "https://www.ncwu.edu.cn/"
+    }, {
+        name: "华水学堂",
+        url: "http://ncwu.fanya.chaoxing.com/portal"
+    }, {
+        name: "图书馆",
+        url: "http://lib.ncwu.edu.cn/"
+    }, {
+        name: "研究生院",
+        url: "http://www5.ncwu.edu.cn/yanjiusheng/"
+    }, {
+        name: "VPN系统",
+        url: "https://vpn.ncwu.edu.cn/"
+    }, {
+        name: "就业信息",
+        url: "https://jyxx.ncwu.edu.cn/ncwu/p/page/index.html"
+    }, {
+        name: "学校贴吧",
+        url: "https://tieba.baidu.com/f?kw=%BB%AA%B1%B1%CB%AE%C0%FB%CB%AE%B5%E7%B4%F3%D1%A7&fr=ala0&tpl=5"
+    } ]
+};
